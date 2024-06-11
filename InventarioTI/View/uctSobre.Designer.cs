9c68095baa8614a18f0f410a27b526d2794510d9
Sergio Agostinho <serginhoagostinho@gmail.com>
2024-06-11 16:44:24 -0300

    Removendo logo da empresa

--- InventarioTI/View/uctSobre.Designer.cs
+++ InventarioTI/View/uctSobre.Designer.cs
@@ -32,8 +32,12 @@
             lblSobre = new Label();
             ptbSobre = new PictureBox();
             ptbSobreSolar = new PictureBox();
+            panel1 = new Panel();
+            label1 = new Label();
+            panel2 = new Panel();
             ((System.ComponentModel.ISupportInitialize)ptbSobre).BeginInit();
             ((System.ComponentModel.ISupportInitialize)ptbSobreSolar).BeginInit();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // lblSobre
@@ -66,11 +70,40 @@
             ptbSobreSolar.TabIndex = 2;
             ptbSobreSolar.TabStop = false;
             // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Black;
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(770, 257);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(278, 94);
+            panel1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(95, 44);
+            label1.Name = "label1";
+            label1.Size = new Size(100, 17);
+            label1.TabIndex = 0;
+            label1.Text = "LOGO EMPRESA";
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Black;
+            panel2.Location = new Point(782, 105);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(189, 20);
+            panel2.TabIndex = 4;
+            // 
             // uctSobre
             // 
             AutoScaleDimensions = new SizeF(7F, 17F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
+            Controls.Add(panel2);
+            Controls.Add(panel1);
             Controls.Add(ptbSobreSolar);
             Controls.Add(ptbSobre);
             Controls.Add(lblSobre);
@@ -79,6 +112,8 @@
             Size = new Size(1084, 473);
             ((System.ComponentModel.ISupportInitialize)ptbSobre).EndInit();
             ((System.ComponentModel.ISupportInitialize)ptbSobreSolar).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -88,5 +123,8 @@
         private Label lblSobre;
         private PictureBox ptbSobre;
         private PictureBox ptbSobreSolar;
+        private Panel panel1;
+        private Label label1;
+        private Panel panel2;
     }
 }
